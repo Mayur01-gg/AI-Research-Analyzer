6920cc06b3421d28dd7a4957481eb1da835dc042
Mayur Chalke <mayur.chalke.dev@gmail.com>
2026-01-10 16:37:46 +0530

    Update core app configuration

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from django.core.management import call_command
 
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        try:
+            call_command('clearsessions')
+        except Exception:
+            # Prevent crash during migrations or startup
+            pass
